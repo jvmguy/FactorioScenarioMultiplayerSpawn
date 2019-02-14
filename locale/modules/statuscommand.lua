25646af2225bd9e38d2f8b2f3fa56c4e9c68c779
Kevin Morgan <jvmguy@gmail.com>
2019-02-14 01:03:41 -0800

    status command sometimes produced an error after removing a player.

--- locale/modules/statuscommand.lua
+++ locale/modules/statuscommand.lua
@@ -1,10 +1,10 @@
 
 local function HoursAndMinutes(ticks)
-    local seconds = ticks/TICKS_PER_SECOND
-    local minutes = math.floor((seconds)/60)
-    local hours = math.floor(minutes/60)
-    minutes = minutes - 60 * hours
-    return string.format("%3d:%02d", hours, minutes)
+    local seconds = ticks/TICKS_PER_SECOND;
+    local minutes = math.floor((seconds)/60);
+    local hours = math.floor(minutes/60);
+    minutes = minutes - 60 * hours;
+    return string.format("%3d:%02d", hours, minutes);
 end
 
 local function StatusCommand_ShowStatus(player, xplayer)
@@ -14,20 +14,25 @@ local function StatusCommand_ShowStatus(player, xplayer)
               HoursAndMinutes(xplayer.online_time),
               math.floor(xplayer.position.x),
               math.floor(xplayer.position.y));
-       game.player.print(status)
+       game.player.print(status);
     end
 end
 
 commands.remove_command("status");
 commands.add_command("status", "shows your location, time in game", function(command)
-    local players = {}
+    local players = {};
     for _,p in pairs(game.players) do
         if p ~= nil then
             players[_] = p;
         end
     end
     table.sort(players, 
-        function(a,b) return a.online_time < b.online_time; end
+        function(a,b)
+            if a~=nil and b~=nil then
+                return a.online_time < b.online_time; 
+            end
+            return false;
+        end
     );
     
     local player = game.player;
